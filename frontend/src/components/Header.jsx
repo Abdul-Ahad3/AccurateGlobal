3bfbad9aaf79bc2845ce88663bf10bc719539bf3
Abdul-Ahad3 <mahad.bscs22seecs@seecs.edu.pk>
2025-12-18 01:45:16 +0500

    fixed header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -25,13 +25,18 @@ function Header() {
             ACCURATE GLOBAL
           </div>
           <ul className="nav-list">
-            <li className="nav-item"><Link to="/">Home</Link></li>
+            <li className="nav-item"><Link to="/" style={{color: "red"}}>Home</Link></li>
             <li className="nav-item">
               <Link to="/#about">About <i className="fas fa-chevron-down"></i></Link>
               <ul className="dropdown">
                 <li><Link to="#">Introduction</Link></li>
                 <li><Link to="#">Accreditation</Link></li>
                 <li><Link to="#">Certification Process</Link></li>
+                <li><Link to="#">Quality & Impartial Policy</Link></li>
+                <li><Link to="#">Appeal & Complaint Handling</Link></li>
+                <li><Link to="#">Suspension & Withdraw</Link></li>
+                <li><Link to="#">Certification Rules</Link></li>
+                <li><Link to="#">Logo Rules</Link></li>
               </ul>
             </li>
             <li className="nav-item">
@@ -40,13 +45,28 @@ function Header() {
                 <li><Link to="#">ISO 9001:2015</Link></li>
                 <li><Link to="#">ISO 14001:2015</Link></li>
                 <li><Link to="#">ISO 45001:2018</Link></li>
+                <li><Link to="#">ISO/IEC 27001:2022</Link></li>
+                <li><Link to="#">ISO 50001:2018</Link></li>
+                <li><Link to="#">ISO 22000:2018</Link></li>
+                <li><Link to="#">ISO/IEC 20000-1:2018</Link></li>
+                <li><Link to="#">ISO 37001:2016</Link></li>
+                <li><Link to="#">ISO 13485:2016</Link></li>
+                <li><Link to="#">ISO 42001:2023</Link></li>
+                <li><Link to="#">ISO 22301:2019</Link></li>
+                <li><Link to="#">ISO/IEC 27701:2019</Link></li>
               </ul>
             </li>
             <li className="nav-item">
               <Link to="#">Services <i className="fas fa-chevron-down"></i></Link>
               <ul className="dropdown">
-                <li><Link to="#">CE Marking</Link></li>
-                <li><Link to="#">HACCP</Link></li>
+                <li><Link to="#">Compliance Certificate  <i className="fas fa-chevron-right"></i></Link></li>
+                <ul className='dropdown'>
+                  <li><Link to="#">CE Marking</Link></li>
+                  <li><Link to="#">HACCP</Link></li>
+                  <li><Link to="#">GMP</Link></li>
+                  <li><Link to="#">RoHS</Link></li>
+                </ul>
+                <li><Link to="#">Governance Risk & Compliance</Link></li>
               </ul>
             </li>
             <li className="nav-item"><Link to="/apply-for-training">Training</Link></li>
